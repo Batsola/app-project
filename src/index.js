7e44a49d88034b3ad2189a498534358944ab1735
Batsola <drawstsu@gmail.com>
2023-07-27 15:51:50 +0300

    updated vanilla app project

--- src/index.js
+++ src/index.js
@@ -1,91 +1,116 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let day = days[now.getDay()];
-let hour = now.getHours().toString().padStart(2, "0");
-let minutes = now.getMinutes().toString().padStart(2, "0");
-let month = months[now.getMonth()];
-let date = now.getDate();
+function formatDate(timestamp) {
+  //calculate the date
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
-let currentMonth = document.querySelector(".date");
-currentMonth.innerHTML = `${date} ${month}`;
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let time = `${day} ${hour}:${minutes}`;
-let currentDay = document.querySelector(".currentDay");
-currentDay.innerHTML = time;
-
-function myRequest(response) {
-  let temperature = document.querySelector(".temp");
-  temperature.innerHTML = Math.round(response.data.main.temp);
-  let newContent = document.querySelector(".currentCity");
-  newContent.innerHTML = response.data.name;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-function userCity(city) {
-  let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(myRequest);
+function currentMonth() {
+  let now = new Date();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let date = now.getDate();
+  let month = months[now.getMonth()];
+  return `${date} ${month}`;
+}
+let currentMonthElement = document.querySelector("#month");
+currentMonthElement.innerHTML = currentMonth();
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  let celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function typedCity(event) {
+function search(city) {
+  let apiKey = "fed392df73b956dce9204cf62bb5cbeb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-input").value;
-  userCity(city);
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
-function myPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  console.log(lat);
-  console.log(lon);
-  let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(myRequest);
-}
-function getMyPosition(event) {
+function displayFahreinheitTemperature(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(myPosition);
+  let temperatureElement = document.querySelector("#temperature");
+  // Remove the active class from the celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function celsiusValue(event) {
+function displayCelsiusTemperature(event) {
   event.preventDefault();
-  let contentCelsius = document.querySelector(".temp");
-  contentCelsius.innerHTML = "12";
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function fahrenheitValue(event) {
-  event.preventDefault();
-  let contentCelsius = document.querySelector(".temp");
-  contentCelsius.innerHTML = "66";
-}
-ceLink.addEventListener("click", celsiusValue);
-faLink.addEventListener("click", fahrenheitValue);
+let celsiusTemperature = null;
 
-let cityName = document.querySelector("#search-form");
-cityName.addEventListener("submit", typedCity);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 let searchButton = document.querySelector("#button-addon1");
-searchButton.addEventListener("click", typedCity);
-let currentLocationButton = document.querySelector("#current-position");
-currentLocationButton.addEventListener("click", getMyPosition);
-userCity("Antananarivo");
+searchButton.addEventListener("click", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahreinheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+search("Antananarivo");
